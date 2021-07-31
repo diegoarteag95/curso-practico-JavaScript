038529fb6383ab5f564dd6ca647d181588918a97
diegoarteaga95 <diego.arteaga95@gmail.com>
2021-07-30 20:10:45 -0500

    se agregan estilos estilos css

--- figuras.js
+++ figuras.js
@@ -1,12 +1,30 @@
 //codigo cuadrado ------------------------------------------------------------
 console.group(`cuadrado`);
+
 function perimetroCuadrado(lado){
-return lado * 4;
+    return lado * 4;
 }
+
 function areaCuadrado(lado){
     return lado * lado;
 }
+
+function calcularPerimetroCuadrado(){
+    let inputLadoCuadrado = document.getElementById(`ladoCuadrado`);
+    valueLado = inputLadoCuadrado.value;
+    let resultadoPerimetroCuadrado = perimetroCuadrado(valueLado);
+    console.log(resultadoPerimetroCuadrado);
+}
+
+function calcularAreaCuadrado(){
+    let inputLadoCuadrado = document.getElementById(`ladoCuadrado`);
+    valueLado = inputLadoCuadrado.value;
+    let resultadoAreaCuadrado = perimetroCuadrado(valueLado);
+    console.log(resultadoAreaCuadrado);
+}
+
 console.groupEnd();
+
 //codigo triangulo -----------------------------------------------------------
 console.group(`triangulo`);
 function perimetroTriangulo(lado1,lado2,base){
@@ -16,6 +34,7 @@ function areaTriangulo(base,altura){
     return (base*altura)/2;
 }
 console.groupEnd();
+
 //codigo circulo -------------------------------------------------------------
 console.group(`circulo`);
 function circunferenciaCirculo(radio){
